3d95f0bb5c032afec4a37044794933ee6c232455
kentaro-maker <kentaro.mo.olesen@gmail.com>
2021-01-29 03:57:17 +0900

    市町村シーダー有効

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(CitySeeder::class);
+        $this->call(CitySeeder::class);
         //$this->call(EventSeeder::class);
-        $this->call(ApplySeeder::class);
+        //$this->call(ApplySeeder::class);
     }
 }
